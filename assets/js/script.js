b9f1d69cc8d9e541655286347b614be120d9a296
MJBauti <michael.bautista@yahoo.com>
2022-08-11 19:09:51 -0700

    results page creation and js whiteboard

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// function to choose which info pulling
+
+// function to change page
+
+// function to fill city, country basic info on left of results page
+
+// function to pull up info on right side
+    // refer to function that decided info
+    // call to each function for specific info
+
+// each function to call specific info
+
