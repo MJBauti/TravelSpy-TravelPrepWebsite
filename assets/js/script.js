0fe3888acafb6624ce9cdc675dfb6d1e892fd3a1
Vader <gassanb97@gmail.com>
2022-08-15 19:33:47 -0700

    updated mods

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,15 @@ cityFormEl.addEventListener('submit', handleCityFormSubmit);
 
 // each function to call specific info
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '069917c6d4msh28e5523d08eaa97p1412bdjsn5f7635020738',
+		'X-RapidAPI-Host': 'hotels-com-provider.p.rapidapi.com'
+	}
+};
+
+fetch('https://hotels-com-provider.p.rapidapi.com/v1/hotels/search?checkin_date=2022-03-26&checkout_date=2022-03-27&sort_order=STAR_RATING_HIGHEST_FIRST&destination_id=1708350&adults_number=1&locale=en_US&currency=USD&children_ages=4%2C0%2C15&price_min=10&star_rating_ids=3%2C4%2C5&accommodation_ids=20%2C8%2C15%2C5%2C1&price_max=500&page_number=1&theme_ids=14%2C27%2C25&amenity_ids=527%2C2063&guest_rating_min=4', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
