c8983a2951e00e1d3e83b28012a5bad74c769f31
MJBauti <michael.bautista@yahoo.com>
2022-08-18 22:12:25 -0700

    buttons working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,91 @@
 var cityFormEl = document.querySelector('#city-form');
+var parisBtn = document.querySelector('#paris-france');
+var londonBtn = document.querySelector('#london-unitedkingdom');
+var tokyoBtn = document.querySelector('#tokyo-japan');
+var newyorkBtn = document.querySelector('#newyork-usa');
+var bangkokBtn = document.querySelector('#bangkok-thailand');
+var seoulBtn = document.querySelector('#seoul-southkorea');
+var deploySpyBtn = document.querySelector('#deploy');
 var currency;
 var countryCode2;
 var timezone;
+var queryString;
 
-function handleCityFormSubmit(event) {
-    event.preventDefault();
 
-    var cityInputVal = document.querySelector('#city-input').value;
+function chooseParis(event) {
+    var cityInputVal = "Paris, France";
+    queryString = './results.html?q=' + cityInputVal;
+    getFranceInfo();
+}
 
+function chooseLondon(event) {
+    var cityInputVal = "London, United Kingdom";
+    queryString = './results.html?q=' + cityInputVal;
+    getUnitedKingdomInfo();
+}
+
+function chooseTokyo(event) {
+    var cityInputVal = "Tokyo, Japan";
+    queryString = './results.html?q=' + cityInputVal;
+    getJapanInfo();
+}
+
+function chooseNewYork(event) {
+    var cityInputVal = "New York, USA";
+    queryString = './results.html?q=' + cityInputVal;
+    getUSAInfo();
+}
+
+function chooseBangkok(event) {
+    var cityInputVal = "Bangkok, Thailand";
+    queryString = './results.html?q=' + cityInputVal;
+    getThailandInfo();
+}
+
+function chooseSeoul(event) {
+    var cityInputVal = "Seoul, South Korea";
+    queryString = './results.html?q=' + cityInputVal;
+    getKoreaInfo();
+}
+
+function deploySpy(event) {
     if (!cityInputVal) {
         console.error('You need a city input value!');
         return;
         // modal.style.display = "block";
         // span.onclick = function() {modal.style.display = "none";}
     }
-
-    var queryString = './results.html?q=' + cityInputVal;
-
     location.assign(queryString);
 }
 
-cityFormEl.addEventListener('submit', handleCityFormSubmit);
+deploySpyBtn.addEventListener('click', deploySpy);
+parisBtn.addEventListener('click', chooseParis);
+londonBtn.addEventListener('click', chooseLondon);
+tokyoBtn.addEventListener('click', chooseTokyo);
+newyorkBtn.addEventListener('click', chooseNewYork);
+bangkokBtn.addEventListener('click', chooseBangkok);
+seoulBtn.addEventListener('click', chooseSeoul);
+
+
 
 var getFranceInfo = function() {
+    localStorage.clear();
     fetch (`https://restcountries.com/v3.1/name/france`, {
         cache: `reload`,
     })
     .then(response => {return response.json();})
     .then(data => {
-        currency = ([0].currencies.EUR.name);
+        currency = (data[0].currencies.EUR.name);
         countryCode2 = (data[0].cca2);
         timezone = (data[0].timezones[0]);
-    })
-}
-
-var getChinaInfo = function() {
-    fetch (`https://restcountries.com/v3.1/name/china`, {
-        cache: `reload`,
-    })
-    .then(response => {return response.json();})
-    .then(data => {
-        currency = ([2].currencies.CNY.name);
-        countryCode2 = (data[2].cca2);
-        timezone = (data[2].timezones[0]);
+        localStorage.setItem("currency", currency);
+        localStorage.setItem("country", countryCode2);
+        localStorage.setItem("timezone", timezone);
     })
 }
 
 var getUnitedKingdomInfo = function() {
+    localStorage.clear();
     fetch (`https://restcountries.com/v3.1/name/gb`, {
         cache: `reload`,
     })
@@ -55,10 +94,14 @@ var getUnitedKingdomInfo = function() {
         currency = (data[0].currencies.GBP.name);
         countryCode2 = (data[0].cca2);
         timezone = (data[0].timezones[0]);
+        localStorage.setItem("currency", currency);
+        localStorage.setItem("country", countryCode2);
+        localStorage.setItem("timezone", timezone);
     })
 }
 
 var getJapanInfo = function() {
+    localStorage.clear();
     fetch (`https://restcountries.com/v3.1/name/japan`, {
         cache: `reload`,
     })
@@ -67,10 +110,14 @@ var getJapanInfo = function() {
         currency = (data[0].currencies.JPY.name);
         countryCode2 = (data[0].cca2);
         timezone = (data[0].timezones[0]);
+        localStorage.setItem("currency", currency);
+        localStorage.setItem("country", countryCode2);
+        localStorage.setItem("timezone", timezone);
     })
 }
 
 var getUSAInfo = function() {
+    localStorage.clear();
     fetch (`https://restcountries.com/v3.1/name/usa`, {
         cache: `reload`,
     })
@@ -79,10 +126,14 @@ var getUSAInfo = function() {
         currency = (data[0].currencies.USD.name);
         countryCode2 = (data[0].cca2);
         timezone = (data[0].timezones[0]);
+        localStorage.setItem("currency", currency);
+        localStorage.setItem("country", countryCode2);
+        localStorage.setItem("timezone", timezone);
     })
 }
 
 var getThailandInfo = function() {
+    localStorage.clear();
     fetch (`https://restcountries.com/v3.1/name/thailand`, {
         cache: `reload`,
     })
@@ -91,10 +142,14 @@ var getThailandInfo = function() {
         currency = (data[0].currencies.THB.name);
         countryCode2 = (data[0].cca2);
         timezone = (data[0].timezones[0]);
+        localStorage.setItem("currency", currency);
+        localStorage.setItem("country", countryCode2);
+        localStorage.setItem("timezone", timezone);
     })
 }
 
-var getCountryInfo = function() {
+var getKoreaInfo = function() {
+    localStorage.clear();
     fetch (`https://restcountries.com/v3.1/name/korea`, {
         cache: `reload`,
     })
@@ -103,5 +158,25 @@ var getCountryInfo = function() {
         currency = (data[1].currencies.KRW.name);
         countryCode2 = (data[1].cca2);
         timezone = (data[1].timezones[0]);
+        localStorage.setItem("currency", currency);
+        localStorage.setItem("country", countryCode2);
+        localStorage.setItem("timezone", timezone);
+    })
+}
+
+// Future get China Country Info
+var getChinaInfo = function() {
+    localStorage.clear();
+    fetch (`https://restcountries.com/v3.1/name/china`, {
+        cache: `reload`,
+    })
+    .then(response => {return response.json();})
+    .then(data => {
+        currency = (data[2].currencies.CNY.name);
+        countryCode2 = (data[2].cca2);
+        timezone = (data[2].timezones[0]);
+        localStorage.setItem("currency", currency);
+        localStorage.setItem("country", countryCode2);
+        localStorage.setItem("timezone", timezone);
     })
 }
